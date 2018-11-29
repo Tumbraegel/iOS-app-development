84c53ae7133cccf67aedf03526b85cb1bcb00b88
Josefine Busch <s0560106@mac7.local>
2018-11-29 12:15:39 +0100

    merged

---
+++ iOS4_Busch_Tumbraegel/iOS4_Busch_Tumbraegel/CurrencyConverter.swift
@@ -0,0 +1,25 @@
+//
+//  Currency.swift
+//  iOS4_Busch_Tumbraegel
+//
+//  Created by Josefine Busch on 26.11.18.
+//  Copyright © 2018 Josefine Busch. All rights reserved.
+//
+
+import Foundation
+
+class CurrencyConverter{
+    var conversionRate: Double
+    
+    init(conversionRate: Double) {
+        self.conversionRate = conversionRate;
+    }
+    
+    func convertToEuro(amount: Double) -> Double {
+        return amount * conversionRate;
+    }
+    
+    func convertFromEuro(amount: Double) -> Double {
+        return amount / conversionRate;
+    }
+}
--- iOS4_Busch_Tumbraegel/iOS4_Busch_Tumbraegel/ViewController.swift
+++ iOS4_Busch_Tumbraegel/iOS4_Busch_Tumbraegel/ViewController.swift
@@ -16,17 +16,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var euroAmount: UITextField!
     @IBOutlet weak var poundAmount: UITextField!
     
-    var calculator = Calculator();
+    let dollarConverter = CurrencyConverter(conversionRate: 1.1449)
+    let poundConverter = CurrencyConverter(conversionRate: 0.8905)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        poundRate.text = "\(calculator.conversionRatePound)";
-        dollarRate.text = "\(calculator.conversionRateDollar)";
+        poundRate.text = "\(poundConverter.conversionRate)";
+        dollarRate.text = "\(dollarConverter.conversionRate)";
     }
     
+    @IBAction func dollarAmountChanged(_ sender: Any) {
+        let euroValue = dollarConverter.convertToEuro(amount: 5.0)
+        euroAmount.text = "\(euroValue)"
+        poundAmount.text = "\(poundConverter.convertFromEuro(amount: euroValue))"
+    }
+    @IBAction func euroAmountChanged(_ sender: Any) {
+        let euroValue = dollarConverter.convertFromEuro(amount: 5.0)
+        dollarAmount.text = "\(euroValue)"
+        poundAmount.text = "\(poundConverter.convertFromEuro(amount: euroValue))"
+    }
+    @IBAction func poundAmountChanged(_ sender: Any) {
+        let euroValue = poundConverter.convertToEuro(amount: 5.0)
+        euroAmount.text = "\(euroValue)"
+        dollarAmount.text = "\(dollarConverter.convertFromEuro(amount: euroValue))"
+    }
     
     @IBAction func dollarRateChanged(_ sender: Any) {
-        
+        print(doublify(text: dollarRate.text))
     }
     
     
